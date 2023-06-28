c1bfa67ac349a4c9de084c375094072b40e96165
VyacheslavGorokhov <vyacheslavgorohov@mail.ru>
2023-06-28 19:53:29 +0700

    Add actions to humanoid

--- ModelsTest/Assets/Scripts/Humanoid/HumanoidActionsController.cs
+++ ModelsTest/Assets/Scripts/Humanoid/HumanoidActionsController.cs
@@ -1,5 +1,6 @@
 namespace Humanoid
 {
+    using Cinemachine;
     using Lerp;
     using Timer;
     using UnityEngine;
@@ -8,8 +9,23 @@ namespace Humanoid
     public class HumanoidActionsController : MonoBehaviour
     {
         [SerializeField]
+        private CharacterController characterController;
+        
+        [SerializeField]
         private Animator animator;
 
+        [SerializeField]
+        private Transform modelTransform;
+        
+        [SerializeField]
+        private Camera raycastCamera;
+        
+        [SerializeField]
+        private CinemachineVirtualCamera aimCamera;
+
+        [SerializeField]
+        private Transform cameraTarget;
+        
         private bool _isInitialized;
         
         private PlayerInputs _playerInputs;
@@ -21,26 +37,57 @@ namespace Humanoid
         private static readonly int YDirection = Animator.StringToHash("YDirection");
 
         [SerializeField]
+        private Vector2Lerp lookInput;
+
+        [SerializeField]
+        private Vector2 lookSensitivity = Vector2.one;
+
+        [SerializeField]
+        private Vector2 aimSensitivity = Vector2.one / 2;
+
+        [SerializeField]
+        private Vector2 verticalAimMinMax = new Vector2(-65,  45);
+        
+        [SerializeField]
+        private float walkSpeed = 0.1f;
+
+        [SerializeField]
         private FloatLerp sprinting;
+
         private static readonly int Sprinting = Animator.StringToHash("Sprinting");
 
         [SerializeField]
         private FloatLerp aiming;
+
         private static readonly int Aiming = Animator.StringToHash("Aiming");
 
         [SerializeField]
         private FloatLerp idling;
-        
+
         [SerializeField]
         private Timer idleTimer;
-        
+
+        [SerializeField]
+        private float aimModelRotationSpeed = 20f;
+
+        [SerializeField]
+        private bool isPrintLog;
+
         private static readonly int Idling = Animator.StringToHash("Idling");
-        
+
         private int _activeActions;
 
         private bool _isFire;
+
         private static readonly int IsFire = Animator.StringToHash("IsFire");
+
         private static readonly int Jump = Animator.StringToHash("Jump");
+
+        private Vector3 _aimTarget;
+
+        private Vector3 _moveForceOfFrame;
+
+        private Vector2 _cameraTargetRotation;
 
         private void OnEnable()
         {
@@ -58,10 +105,71 @@ namespace Humanoid
             Unsubscribe();
         }
 
+        private void FixedUpdate()
+        {
+            ApplyForceToCharacterController();
+        }
+
         private void Update()
         {
             UpdateValueLerps();
             UpdateIdleState();
+
+            RotateModel();
+        }
+
+        private void RotateModel()
+        {
+            if (IsNeedRotateModel() == false)
+            {
+                return;
+            }
+
+            if (Raycast(out var hit) == false)
+            {
+                return;
+            }
+
+            var position = modelTransform.position;
+
+            hit.y = position.y;
+            var direction = (hit - position).normalized;
+
+            DebugLog("Hit: " + hit + " AimDirection: " + direction);
+
+            modelTransform.forward = Vector3.Lerp
+            (
+                modelTransform.forward,
+                direction,
+                Time.deltaTime * aimModelRotationSpeed
+            );
+        }
+
+        private bool IsNeedRotateModel()
+        {
+            return aiming.Value != 0 || directionInput.X != 0 || directionInput.Y != 0;
+        }
+
+        private void ApplyForceToCharacterController()
+        { 
+            characterController.Move(_moveForceOfFrame);
+        }
+
+        private bool Raycast(out Vector3 hit)
+        {
+            var screenCenterPoint = new Vector2((float)Screen.width / 2, (float)Screen.height / 2);
+            var ray = raycastCamera.ScreenPointToRay(screenCenterPoint);
+
+            if (Physics.Raycast(ray, out var raycastHit, 1500))
+            {
+                hit = raycastHit.point;
+
+                return true;
+            }
+
+            hit = Vector3.zero;
+            
+            return false;
         }
 
         private void UpdateIdleState()
@@ -81,8 +189,9 @@ namespace Humanoid
         {
             var deltaTime = Time.deltaTime;
             
-            idling.UpdateValue(deltaTime);
             directionInput.UpdateValue(deltaTime);
+            lookInput.UpdateValue(deltaTime);
+            idling.UpdateValue(deltaTime);
             sprinting.UpdateValue(deltaTime);
             aiming.UpdateValue(deltaTime);
         }
@@ -107,6 +216,12 @@ namespace Humanoid
             _actions.Walk.canceled += WalkInput;
 
             directionInput.OnValueChanged += Walk;
+            
+            _actions.Look.started += LookInput;
+            _actions.Look.performed += LookInput;
+            _actions.Look.canceled += LookInput;
+            
+            lookInput.OnValueChanged += Look;
             
             _actions.Sprint.started += SprintInput;
             _actions.Sprint.canceled += SprintInput;
@@ -135,6 +250,12 @@ namespace Humanoid
 
             directionInput.OnValueChanged -= Walk;
             
+            _actions.Look.started -= LookInput;
+            _actions.Look.performed -= LookInput;
+            _actions.Look.canceled -= LookInput;
+            
+            lookInput.OnValueChanged -= Look;
+            
             _actions.Sprint.started -= SprintInput;
             _actions.Sprint.canceled -= SprintInput;
             
@@ -159,23 +280,98 @@ namespace Humanoid
 
             Walk();
             
-            if (context.started)
-            {
-                _activeActions++;
-            }
-
-            if (context.canceled)
-            {
-                _activeActions--;
-            }
+            CheckActionPhase(context);
         }
 
         private void Walk()
         {
+            var targetRotation = Mathf.Atan2
+              (
+                  directionInput.X,
+                  directionInput.Y
+              )
+              * Mathf.Rad2Deg
+              + cameraTarget.eulerAngles.y;
+
+            var targetDirection = 
+                Quaternion.Euler(0.0f, targetRotation, 0.0f) 
+                * Vector3.forward;
+
+            var inputMagnitude = Mathf.Clamp01(directionInput.Value.magnitude);
+            var sprintMultiplier = 1 + sprinting.Value;
+            var currentSpeed = walkSpeed * inputMagnitude * sprintMultiplier;
+            
+            _moveForceOfFrame = targetDirection.normalized * currentSpeed;
+
             animator.SetFloat(XDirection, directionInput.X);
             animator.SetFloat(YDirection, directionInput.Y);
 
-            Debug.Log("Walk: " + directionInput.Value);
+            DebugLog("Walk: " + directionInput.Value);
+        }
+
+        private void LookInput(InputAction.CallbackContext context)
+        {
+            var look = context.ReadValue<Vector2>();
+            lookInput.SetTargetValue(look);
+
+            Look();
+            Walk();
+            
+            CheckActionPhase(context);
+        }
+
+        private void Look()
+        {
+            GetLookInputsWithSensitivity(out var xInput, out var yInput);
+
+            SetNewCameraRotation(xInput, yInput);
+
+            DebugLog("Look: " + lookInput.Value);
+        }
+
+        private void SetNewCameraRotation(float xInput, float yInput)
+        {
+            _cameraTargetRotation.x += xInput;
+            _cameraTargetRotation.y += yInput;
+
+            ClampCameraTargetRotation();
+
+            cameraTarget.rotation = Quaternion.Euler
+                (_cameraTargetRotation.y, _cameraTargetRotation.x, 0.0f);
+        }
+
+        private void GetLookInputsWithSensitivity(out float xInput, out float yInput)
+        {
+            xInput = lookInput.Value.x;
+            yInput = -lookInput.Value.y;
+
+            if (aiming.Value == 0)
+            {
+                xInput *= lookSensitivity.x;
+                yInput *= lookSensitivity.y;
+                
+                return;
+            }
+
+            xInput *= aimSensitivity.x;
+            yInput *= aimSensitivity.y;
+        }
+
+        private void ClampCameraTargetRotation()
+        {
+            _cameraTargetRotation.x %= 360;
+
+            _cameraTargetRotation.x = 
+                _cameraTargetRotation.x > 180
+                ? _cameraTargetRotation.x - 360
+                : _cameraTargetRotation.x;
+
+            _cameraTargetRotation.x = 
+                _cameraTargetRotation.x < -180
+                ? _cameraTargetRotation.x + 360
+                : _cameraTargetRotation.x;
+
+            _cameraTargetRotation.y = Mathf.Clamp(_cameraTargetRotation.y, verticalAimMinMax.x, verticalAimMinMax.y);
         }
 
         private void SprintInput(InputAction.CallbackContext context)
@@ -185,22 +381,14 @@ namespace Humanoid
 
             Sprint();
             
-            if (context.started)
-            {
-                _activeActions++;
-            }
-
-            if (context.canceled)
-            {
-                _activeActions--;
-            }
+            CheckActionPhase(context);
         }
 
         private void Sprint()
         {
             animator.SetFloat(Sprinting, sprinting.Value);
             
-            Debug.Log("Sprinting: " + sprinting.Value);
+            DebugLog("Sprinting: " + sprinting.Value);
         }
 
         private void AimInput(InputAction.CallbackContext context)
@@ -208,65 +396,39 @@ namespace Humanoid
             var aimingValue = context.ReadValue<float>();
             aiming.SetTargetValue(aimingValue);
 
+            aimCamera.gameObject.SetActive(aimingValue > 0);
+            
             Aim();
             
-            if (context.started)
-            {
-                _activeActions++;
-            }
-
-            if (context.canceled)
-            {
-                _activeActions--;
-            }
+            CheckActionPhase(context);
         }
 
         private void Aim()
         {
             animator.SetFloat(Aiming, aiming.Value);
             
-            Debug.Log("Aiming: " + aiming.Value);
+            DebugLog("Aiming: " + aiming.Value);
         }
 
         private void JumpInput(InputAction.CallbackContext context)
         {
             DoJump();
             
-            if (context.started)
-            {
-                _activeActions++;
-            }
-
-            if (context.canceled)
-            {
-                _activeActions--;
-            }
+            CheckActionPhase(context);
         }
 
         private void DoJump()
         {
-            Debug.Log("Jump");
+            DebugLog("Jump");
             
             animator.SetTrigger(Jump);
         }
         
         private void FireInput(InputAction.CallbackContext context)
         {
-            if (context.started)
-            {
-                _isFire = true;
-                
-                _activeActions++;
-            }
+            _isFire = CheckActionPhase(context);
 
-            if (context.canceled)
-            {
-                _isFire = false;
-                
-                _activeActions--;
-            }
-
-            Debug.Log("Fire: " + _isFire);
+            DebugLog("Fire: " + _isFire);
             
             Fire();
         }
@@ -284,6 +446,33 @@ namespace Humanoid
         private void Idle()
         {
             animator.SetFloat(Idling, idling.Value);
+        }
+
+        private bool CheckActionPhase(InputAction.CallbackContext context)
+        {
+            if (context.started)
+            {
+                _activeActions++;
+
+                return true;
+            }
+
+            if (context.canceled)
+            {
+                _activeActions--;
+
+                return false;
+            }
+            
+            return true;
+        }
+
+        private void DebugLog(string message)
+        {
+            if (isPrintLog)
+            {
+                Debug.Log(message);
+            }
         }
     }
 }
